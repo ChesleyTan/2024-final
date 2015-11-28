1c3c1d031ecc3c1cf7e253f003653c0c8143ce97
Chesley Tan <chesleytan97@gmail.com>
2015-11-28 16:23:20 -0500

    Add function to handle the add command
    Add function to flush stdin

--- main.cpp
+++ main.cpp
@@ -10,29 +10,69 @@ int main() {
     while (!is_eof) {
         cout << PROMPT;
         getline(cin, line);
-        is_eof = cin.eof();
+        is_eof = cin.eof(); // Stop when EOF is reached
         if (is_eof) break;
         trim_trailing_whitespace(line);
+        if (line == "") continue; // Ignore empty lines
+#ifdef DEBUG
         cout << "You entered: " << line << endl;
+#endif
         if (line == "help") {
+#ifdef DEBUG
             cout << "Got help command" << endl;
+#endif
         }
         else if (line == "add") {
+#ifdef DEBUG
             cout << "Got add command" << endl;
+            handle_add_cmd();
+#endif
         }
         else if (line == "log") {
+#ifdef DEBUG
             cout << "Got log command" << endl;
+#endif
         }
         else if (line == "summary") {
+#ifdef DEBUG
             cout << "Got summary command" << endl;
+#endif
         }
         else {
+#ifdef DEBUG
             cout << "Unrecognized command: " << line << endl;
+#endif
         }
     }
     return 0;
 }
 
+void handle_add_cmd() {
+    // TODO validate inputs
+    string payer, debtor, description;
+    double amount;
+    cout << "Payer: ";
+    cin >> payer;
+    cout << "Debtor: ";
+    cin >> debtor;
+    cout << "Amount: ";
+    cin >> amount;
+    cout << "Description: ";
+    cin >> description;
+#ifdef DEBUG
+    cout << "Payer: " << payer << endl;
+    cout << "Debtor: " << debtor << endl;
+    cout << "Amount: " << std::fixed << std::setprecision(2) << amount << endl;
+    cout << "Description: " << description << endl;
+#endif
+    flush_cin();
+}
+
 void trim_trailing_whitespace(string &s) {
     s.erase(s.find_last_not_of(" \t\r\n") + 1);
+}
+
+void flush_cin() {
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
 }
--- main.h
+++ main.h
@@ -3,7 +3,11 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <limits>
 
 void trim_trailing_whitespace(std::string &s);
+void handle_add_cmd();
+void flush_cin();
 
 #endif /* MAIN_H_ */
