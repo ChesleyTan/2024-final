578cbc9339aacc6d7f63fcb0fc650aa9a81f27a8
Chesley Tan <chesleytan97@gmail.com>
2015-11-28 15:49:39 -0500

    Implement basic command parsing
    Trim trailing whitespace from input line

--- main.cpp
+++ main.cpp
@@ -1,17 +1,38 @@
-#include <iostream>
-#include <string>
+#include "main.h"
 using namespace std;
 
 const char *PROMPT = ">> ";
 int main() {
     string line;
-    bool isEof = false;
-    while (!isEof) {
+    bool is_eof = false;
+    cout << "== Expense/Debt Tracker by Chesley Tan, Qingjun Wang, and Shaoke Xu ==" << endl;
+    cout << "Enter `help` for a list of commands" << endl;
+    while (!is_eof) {
         cout << PROMPT;
         getline(cin, line);
-        isEof = cin.eof();
-        if (isEof) break;
+        is_eof = cin.eof();
+        if (is_eof) break;
+        trim_trailing_whitespace(line);
         cout << "You entered: " << line << endl;
+        if (line == "help") {
+            cout << "Got help command" << endl;
+        }
+        else if (line == "add") {
+            cout << "Got add command" << endl;
+        }
+        else if (line == "log") {
+            cout << "Got log command" << endl;
+        }
+        else if (line == "summary") {
+            cout << "Got summary command" << endl;
+        }
+        else {
+            cout << "Unrecognized command: " << line << endl;
+        }
     }
     return 0;
+}
+
+void trim_trailing_whitespace(string &s) {
+    s.erase(s.find_last_not_of(" \t\r\n") + 1);
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <iostream>
+#include <string>
+
+void trim_trailing_whitespace(std::string &s);
+
+#endif /* MAIN_H_ */
