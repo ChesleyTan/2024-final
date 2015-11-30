a1da05e356d4206a51b64c26a2d81a42d5051e42
helloworldtoday <helloworldtoday@users.noreply.github.com>
2015-11-29 22:17:02 -0500

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -11,3 +11,15 @@ Node::Node(string payer_name, string debtor_name, double val) {
     this->debtor_name = debtor_name;
     this->val = val;
 }
+
+string Node::getPayerName() {
+    return payer_name;
+}
+
+string Node::getDebtorName() {
+    return debtor_name;
+}
+
+double Node::getVal() {
+    return val;
+}
